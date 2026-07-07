14f8d8fa0320b0542278ef683e3c8e45fb5729dc
Faiza Hossain <faizahossain.tech@gmail.com>
2026-07-07 19:28:13 +0600

    Time: 1141 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select e1.name
+from Employee as e1
+JOIN Employee e2 on e1.id=e2.managerId
+where e1.id=e2.managerId and e2.managerId IS NOT NULL
+group by e2.managerId
+having COUNT(e2.managerId)>=5
