fd3fbf139a6cea4a91f0c5731c647278048ee54c
Faiza Hossain <faizahossain.tech@gmail.com>
2026-07-16 20:29:13 +0600

    Time: 759 ms (40.17%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+select s.product_id,
+s.year as first_year,s.quantity,s.price
+from Sales s
+JOIN (select product_id,min(year) as first_year
+from Sales 
+group by product_id) t
+on s.product_id=t.product_id
+and s.year=t.first_year
