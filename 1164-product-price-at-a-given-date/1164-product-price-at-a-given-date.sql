6f8f985f6e07c897805d6464720ad66f31142421
Faiza Hossain <faizahossain.tech@gmail.com>
2026-07-19 21:53:49 +0600

    Time: 879 ms (10.12%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,14 +1,12 @@
-SELECT t.product_id, IFNULL(p.new_price, 10) AS price
-FROM (
-    SELECT DISTINCT product_id
-    FROM Products
-) t
-LEFT JOIN Products p
-    ON p.product_id = t.product_id
-    AND p.change_date = (
-        SELECT MAX(change_date)
-        FROM Products
-        WHERE product_id = t.product_id
-        AND change_date <= '2019-08-16'
-    )
-ORDER BY t.product_id;
+SELECT p.product_id,
+COALESCE(
+    (
+    select p1.new_price
+    from Products as p1
+    where p.product_id=p1.product_id
+         and p1.change_date<='2019-08-16'
+    order by p1.change_date DESC
+    limit 1
+), 10) as price
+From (select distinct product_id
+from Products) as p
