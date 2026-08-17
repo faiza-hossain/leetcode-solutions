389985f5c60352b11048dbeb98d443397d3c7691
Faiza Hossain <faizahossain.tech@gmail.com>
2026-08-17 11:43:02 +0600

    Time: 2115 ms (58.82%), Space: 53.8 MB (37.25%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.py
+++ 1563-stone-game-v/1563-stone-game-v.py
@@ -4,8 +4,6 @@ from itertools import accumulate
 class Solution:
     def stoneGameV(self, stoneValue):
         n = len(stoneValue)
-
-        # Prefix sums
         prefix = list(accumulate(stoneValue, initial=0))
 
         @cache
@@ -22,21 +20,18 @@ class Solution:
                 right -= stoneValue[k]
 
                 if left < right:
-                    # Bob removes right
                     if ans >= left * 2:
                         continue
 
                     ans = max(ans, left + dp(i, k))
 
                 elif left > right:
-                    # Bob removes left
                     if ans >= right * 2:
                         break
 
                     ans = max(ans, right + dp(k + 1, j))
 
                 else:
-                    # Equal: Alice chooses
                     ans = max(
                         ans,
                         left + dp(i, k),
