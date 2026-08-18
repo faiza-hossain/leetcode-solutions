821189989ce8957438e7390b313db67ede478739
Faiza Hossain <faizahossain.tech@gmail.com>
2026-08-18 20:59:21 +0600

    Time: 3 ms (62.3%), Space: 19.2 MB (75.41%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,15 +1,11 @@
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
         count = {}
-
-        # Check every subarray of size k
         for i in range(len(nums) - k + 1):
             window = set(nums[i:i + k])
 
             for x in window:
                 count[x] = count.get(x, 0) + 1
-
-        # Largest number appearing in exactly one window
         ans = -1
 
         for x, freq in count.items():
