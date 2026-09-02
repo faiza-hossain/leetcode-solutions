807990369b8dd45116ed6adc6ff194025487067c
Faiza Hossain <faizahossain.tech@gmail.com>
2026-09-02 10:32:41 +0600

    Time: 7 ms (1.01%), Space: 19.3 MB (13.38%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -2,7 +2,6 @@ class Solution:
     def uniformArray(self, nums1: List[int]) -> bool:
         n = len(nums1)
         par = [x % 2 for x in nums1]
-
         def feasible(target: int) -> bool:
             for i in range(n):
                 if par[i] == target:
@@ -12,5 +11,4 @@ class Solution:
                     continue
                 return False
             return True
-
         return feasible(0) or feasible(1)
